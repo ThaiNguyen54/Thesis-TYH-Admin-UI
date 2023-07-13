1212cf77c282d312d1661bbd9b8dd2b5ac5f42fa
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2023-07-13 14:00:32 +0700

    formatted project

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,69 +1,88 @@
-import "./new.scss";
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import './new.scss';
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const New = ({ inputs, title }) => {
-  const [file, setFile] = useState("");
-  const [inputData, setInputData] = useState({Name: '',  Des: ''})
-  const navigate = useNavigate()
-
+const New = ({ title }) => {
+  const [file, setFile] = useState('');
+  const [inputData, setInputData] = useState({ Name: '', Des: '' });
+  const navigate = useNavigate();
 
   function handleSubmit(event) {
-    event.preventDefault()
-    axios.post('http://localhost:3001/ver1/hairstyle/' + file.name, inputData)
-        .then(res => {
-          alert('Data Added Successfully');
-          navigate('/')
-        }).catch(err => console.log(err))
+    event.preventDefault();
+    axios.post(`http://localhost:3001/ver1/hairstyle/${file.name}`, inputData)
+      .then(() => {
+        alert('Data Added Successfully');
+        navigate('/');
+      }).catch((err) => console.log(err));
   }
 
   return (
+  // eslint-disable-next-line react/react-in-jsx-scope
     <div className="new">
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <div className="newContainer">
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <div className="top">
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           <h1>{title}</h1>
         </div>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <div className="bottom">
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           <div className="left">
+            {/* eslint-disable-next-line react/react-in-jsx-scope */}
             <img
               src={
                 file
                   ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                  : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
               }
               alt=""
             />
           </div>
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           <div className="right">
+            {/* eslint-disable-next-line react/react-in-jsx-scope */}
             <form onSubmit={handleSubmit}>
+              {/* eslint-disable-next-line react/react-in-jsx-scope */}
               <div className="formInput">
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
                 <label htmlFor="file">
+                  {/* eslint-disable-next-line react/react-in-jsx-scope */}
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
                 <input
                   type="file"
                   id="file"
                   onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
+                  style={{ display: 'none' }}
                 />
               </div>
 
-              <div className="formInput" key='1'>
+              {/* eslint-disable-next-line react/react-in-jsx-scope */}
+              <div className="formInput" key="1">
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control,react/react-in-jsx-scope */}
                 <label>Name</label>
-                <input type='text' onChange={e => setInputData({...inputData, Name: e.target.value})} />
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
+                <input type="text" onChange={(e) => setInputData({ ...inputData, Name: e.target.value })} />
               </div>
 
-              {/*<div className="formInput" key='2'>*/}
-              {/*  <label>Url</label>*/}
-              {/*  <input type='text' onChange={e => setInputData({...inputData, Url: e.target.value})} />*/}
-              {/*</div>*/}
+              {/* <div className="formInput" key='2'> */}
+              {/*  <label>Url</label> */}
+              {/*  <input type='text' onChange={e => setInputData({...inputData, Url: e.target.value})} /> */}
+              {/* </div> */}
 
-              <div className="formInput" key='3'>
+              {/* eslint-disable-next-line react/react-in-jsx-scope */}
+              <div className="formInput" key="3">
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control,react/react-in-jsx-scope */}
                 <label>Description</label>
-                <input type='text' onChange={e => setInputData({...inputData, Des: e.target.value})} />
+                {/* eslint-disable-next-line react/react-in-jsx-scope */}
+                <input type="text" onChange={(e) => setInputData({ ...inputData, Des: e.target.value })} />
               </div>
+              {/* eslint-disable-next-line react/button-has-type,react/react-in-jsx-scope */}
               <button>Add</button>
             </form>
           </div>
