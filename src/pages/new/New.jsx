34ab464f36023e3d66734089d2134eb9753b1df7
ThaiNguyen54 <59272463+ThaiNguyen54@users.noreply.github.com>
2023-10-11 12:33:04 +0700

    update add hairstyle ui

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,13 +1,21 @@
-import './new.scss';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import {Form, Input, Button, Space} from 'antd'
+import {Header} from "../../components";
+import { message, Upload } from "antd";
+import {blue} from "@mui/material/colors";
+
 
 const New = ({ title }) => {
   const [file, setFile] = useState('');
   const [inputData, setInputData] = useState({ Name: '', Des: '' });
   const navigate = useNavigate();
+
+  const FormItem = Form.Item;
+  const { TextArea } = Input;
+  const { Dragger } = Upload;
 
   const handleSelectFile = (e) => setFile(e.target.files[0]);
 
@@ -17,13 +25,15 @@ const New = ({ title }) => {
     try {
       const data = new FormData();
       data.append("my_file", file);
+      console.log('this is file')
+      console.log(file)
       data.name = file.name;
       data.append("Name", inputData.Name)
       data.append("Des", inputData.Des)
       // data.Name = inputData.Name;
       // data.Des = inputData.Des;
       console.log(data)
-      const res = await axios.post("http://localhost:7000/ver1/hairstyle/", data).then(result => {
+      const res = await axios.post("https://geminisoftvn.ddns.net:7001/ver1/hairstyle", data).then(result => {
         alert("Added a new hairstyle successfully")
         navigate('/Hair')
       });
@@ -32,6 +42,46 @@ const New = ({ title }) => {
       alert(error.message);
     }
   }
+
+
+  return (
+      <div className="New">
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+          <Header title="Add new hairstyle"/>
+          <Form.Item align="center" style={{ width: "100%"}} label="Hairstyle name">
+            <Input onChange={(e) => setInputData({ ...inputData, Name: e.target.value })}/>
+          </Form.Item>
+
+          <Form.Item style={{ width: "100%"}} label="Hairstyle description">
+            <TextArea rows={4} onChange={(e) => setInputData({ ...inputData, Des: e.target.value })}/>
+          </Form.Item>
+
+          <Form.Item style={{ width: "50%"}} label="Upload hairstyle image">
+            <input type="file" name="file" id="file" onChange={ handleSelectFile } multiple={false}/>
+          </Form.Item>
+
+          <Form.Item>
+            <img style={{ width: "20%", height: "20%"}}
+                 src={
+                   file
+                       ? URL.createObjectURL(file)
+                       : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+                 }
+                 alt=""
+            />
+          </Form.Item>
+
+          <Form.Item label="">
+            <Button type="default" htmlType="submit" style={{ width: "10%"}} onClick={ handleSubmit }>
+              Add
+            </Button>
+          </Form.Item>
+        </div>
+
+      </div>
+
+  );
+
 
   return (
   // eslint-disable-next-line react/react-in-jsx-scope
