baef75319ed694c69c3eb7ca4e4fd45c4c940596
ThaiNguyen54 <59272463+ThaiNguyen54@users.noreply.github.com>
2023-12-22 10:02:46 +0700

    update apis

--- src/api/api.js
+++ src/api/api.js
@@ -1,37 +1,37 @@
 const endpoint = 'shair-engine/ver1'
 const authEndpoint = 'shair-engine/ver1/auth'
 
-// const api = {
-//     "LOGIN": `https://geminisoftvn.ddns.net:7001/${endpoint}/admin/login`,
-//     "ADD_HAIR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
-//     "UPDATE_HAIR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
-//     "DELETE_HAIR":`https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
-//     "GENERATE_HAIR": `https://geminisoftvn.ddns.net:7001/${endpoint}/generate_hair`,
-//     "GET_HAIR": `https://geminisoftvn.ddns.net:7001/${endpoint}/hairstyle`,
-//     "SET_TRENDING": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle/trending`,
-//     "UPDATE_PASS": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin/pass`,
-//     "UPDATE_DISPLAYNAME": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
-//     "UPDATE_AVATAR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
-//     "UPDATE_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
-//     "DELETE_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
-//     "GET_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
-// }
-
 const api = {
-    "LOGIN": "https://localhost:7001/shair-engine/ver1/admin/login",
-    "ADD_HAIR": `https://localhost:7001/${authEndpoint}/hairstyle`,
-    "UPDATE_HAIR": `https://localhost:7001/${authEndpoint}/hairstyle`,
-    "DELETE_HAIR":`https://localhost:7001/${authEndpoint}/hairstyle`,
-    "GENERATE_HAIR": "https://localhost:7001/ver1/generate_hair",
-    "GET_HAIR": `https://localhost:7001/${endpoint}/hairstyle`,
-    "SET_TRENDING": `https://localhost:7001/${authEndpoint}/hairstyle/trending`,
-    "UPDATE_PASS": `https://localhost:7001/${authEndpoint}/admin/pass`,
-    "UPDATE_DISPLAYNAME": `https://localhost:7001/${authEndpoint}/admin`,
-    "UPDATE_AVATAR": `https://localhost:7001/${authEndpoint}/admin`,
-    "UPDATE_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
-    "DELETE_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
-    "GET_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
+    "LOGIN": `https://geminisoftvn.ddns.net:7001/${endpoint}/admin/login`,
+    "ADD_HAIR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
+    "UPDATE_HAIR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
+    "DELETE_HAIR":`https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle`,
+    "GENERATE_HAIR": `https://geminisoftvn.ddns.net:7001/${endpoint}/generate_hair`,
+    "GET_HAIR": `https://geminisoftvn.ddns.net:7001/${endpoint}/hairstyle`,
+    "SET_TRENDING": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/hairstyle/trending`,
+    "UPDATE_PASS": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin/pass`,
+    "UPDATE_DISPLAYNAME": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
+    "UPDATE_AVATAR": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
+    "UPDATE_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
+    "DELETE_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
+    "GET_ADMIN": `https://geminisoftvn.ddns.net:7001/${authEndpoint}/admin`,
 }
+
+// const api = {
+//     "LOGIN": "https://localhost:7001/shair-engine/ver1/admin/login",
+//     "ADD_HAIR": `https://localhost:7001/${authEndpoint}/hairstyle`,
+//     "UPDATE_HAIR": `https://localhost:7001/${authEndpoint}/hairstyle`,
+//     "DELETE_HAIR":`https://localhost:7001/${authEndpoint}/hairstyle`,
+//     "GENERATE_HAIR": "https://localhost:7001/ver1/generate_hair",
+//     "GET_HAIR": `https://localhost:7001/${endpoint}/hairstyle`,
+//     "SET_TRENDING": `https://localhost:7001/${authEndpoint}/hairstyle/trending`,
+//     "UPDATE_PASS": `https://localhost:7001/${authEndpoint}/admin/pass`,
+//     "UPDATE_DISPLAYNAME": `https://localhost:7001/${authEndpoint}/admin`,
+//     "UPDATE_AVATAR": `https://localhost:7001/${authEndpoint}/admin`,
+//     "UPDATE_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
+//     "DELETE_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
+//     "GET_ADMIN": `https://localhost:7001/${authEndpoint}/admin`,
+// }
 
 
 
